8bb8a9cb0a30cfc418454251cd90720505e95a62
GMuchoki <muchokigeoffreymunene@gmail.com>
2025-08-15 14:34:20 +0300

    Fetch character data from API instead of static object
    Replaces the hardcoded character data with a fetch request to the Rick and Morty API. This allows the app to dynamically load and display character information from the API.

--- script.js
+++ script.js
@@ -1,37 +1,18 @@
-const data = {
-  characters: [
-    {
-      name: "Rick Sanchez",
-      status: "Alive",
-      species: "Human",
-      image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-    },
-    {
-      name: "Morty Smith",
-      status: "Alive",
-      species: "Human",
-      image: "https://rickandmortyapi.com/api/character/avatar/2.jpeg"
-    },
-    {
-      name: "Summer Smith",
-      status: "Alive",
-      species: "Human",
-      image: "https://rickandmortyapi.com/api/character/avatar/3.jpeg"
-    }
-  ]
-};
-
-
 const charactersContainer = document.getElementById("characters-container");
 
-data.characters.forEach(function(item){
-  let newElement = document.createElement("div");
-  newElement.setAttribute('class', 'card');
-  charactersContainer.appendChild(newElement);
-  newElement.innerHTML = `
-  <img src=${item.image} alt=${item.name} class="character-image"> 
-  <p class="character-detail"><span class="attribute">Name:</span> ${item.name}</p>
-  <p class="character-detail"><span class="attribute">Status:</span> ${item.status}</p> 
-  <p class="character-detail"><span class="attribute">Species:</span> ${item.species}</p>
-  `;
+fetch("https://rickandmortyapi.com/api/character")
+.then(results => results.json())
+.then(data => {
+  data.results.forEach(function(item){
+    let newElement = document.createElement("div");
+    newElement.setAttribute('class', 'card');
+    charactersContainer.appendChild(newElement);
+    newElement.innerHTML = `
+    <img src=${item.image} alt=${item.name} class="character-image"> 
+    <p class="character-detail"><span class="attribute">Name:</span> ${item.name}</p>
+    <p class="character-detail"><span class="attribute">Status:</span> ${item.status}</p> 
+    <p class="character-detail"><span class="attribute">Species:</span> ${item.species}</p>
+    `;
+  });
 });
+
